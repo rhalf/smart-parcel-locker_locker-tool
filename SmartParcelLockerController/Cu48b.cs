8f22d4b28d61620f938a78ad0886b7fe64c881e9
Rhalf Wendel David Caacbay <rhalfcaacbay@gmail.com>
2022-01-02 16:46:31 +0800

    Initial commit

---
+++ SmartParcelLockerController/Cu48b.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartParcelLockerController
+{
+    internal class Cu48b
+    {
+        SerialPort _serialPort;
+        byte[] _dataOut;
+        byte[] _dataIn;
+        BitArray _lockers;
+        BitArray _sensors;
+
+        public static int ADDRESS_ALL = 0x0A;
+        public static int LOCKER_ALL = 0x30;
+        public static int GET_STATUS = 0x60;
+        public static int LOCKER_UNLOCK = 0x61;
+        public static int LOCKER_STATUS = 0x75;
+
+
+        public delegate void OnDataReceivedHandler(object sender, EventArgs e);
+        public event OnDataReceivedHandler onDataReceived;
+
+        public Cu48b(String portName, int baudrate)
+        {
+            try
+            {
+                _serialPort = new SerialPort();
+                _serialPort.PortName = portName;
+                _serialPort.BaudRate = baudrate;
+                _serialPort.StopBits = StopBits.One;
+                _serialPort.Handshake = Handshake.None;
+                _serialPort.Parity = Parity.None;
+                _serialPort.DataBits = 8;
+                _serialPort.DtrEnable = false;
+                _serialPort.RtsEnable = false;
+
+                _serialPort.ReadTimeout = 3000;
+                _serialPort.WriteTimeout = 3000;
+
+                _serialPort.ReadBufferSize = 18;
+                _serialPort.WriteBufferSize = 6;
+
+                _serialPort.DataReceived += this.dataReceived;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void open()
+        {
+            if (_serialPort != null)
+                if (!_serialPort.IsOpen)
+                    _serialPort.Open();
+        }
+
+        public void close()
+        {
+            if (_serialPort != null)
+                if (_serialPort.IsOpen)
+                {
+                    _serialPort.Close();
+                    _serialPort.Dispose();
+                    _serialPort = null;
+                }
+        }
+
+        public void dataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int bytesToRead = _serialPort.BytesToRead;
+            if (bytesToRead == 18)
+            {
+                _dataIn = new byte[bytesToRead];
+                _serialPort.Read(_dataIn, 0, bytesToRead);
+                parse(_dataIn);
+
+                if (onDataReceived != null)
+                {
+                    onDataReceived(this, new EventArgs());
+                }
+            }
+        }
+
+
+        public void send(int address, int lockerNumber, int command)
+        {
+            _dataOut = new byte[6];
+            _dataOut[0] = 0x02;
+            _dataOut[1] = (byte)address;
+            _dataOut[2] = (byte)lockerNumber;
+            _dataOut[3] = (byte)command;
+            _dataOut[4] = 0x03;
+            _dataOut[5] = checkSum(_dataOut);
+
+            if (_serialPort != null)
+                if (_serialPort.IsOpen)
+                    _serialPort.Write(_dataOut, 0, _dataOut.Length);
+
+        }
+
+        public byte checkSum(byte[] datas)
+        {
+            int sum = 0;
+            foreach (byte data in datas)
+            {
+                sum = sum + (int)data;
+            }
+            return (byte)(sum & 0xFF);
+        }
+
+        public byte[] dataOut
+        {
+            get { return _dataOut; }
+        }
+
+        public byte[] dataIn
+        {
+            get { return _dataIn; }
+        }
+
+        public BitArray lockers
+        {
+            get { return _lockers; }
+        }
+        public BitArray sensors
+        {
+            get { return _sensors; }
+        }
+        public void parse(byte[] datas)
+        {
+            if (datas == null) throw new Exception("Data is null");
+            if (datas.Length < 18) throw new Exception("Data is short");
+
+            byte[] cloned = (byte[])datas.Clone();
+            int prevCheckSum = cloned[17];
+            cloned[17] = 0;
+
+            int currentCheckSum = checkSum(cloned);
+            if (prevCheckSum != currentCheckSum) throw new Exception("CheckSum is wrong");
+
+            if (datas[3] != 0x75) throw new Exception("Wrong command");
+
+            _lockers = new BitArray(new byte[] {
+                datas[4],
+                datas[5],
+                datas[6],
+                datas[7],
+                datas[8],
+                datas[9],
+            });
+
+            _sensors = new BitArray(new byte[] {
+                datas[10],
+                datas[11],
+                datas[12],
+                datas[13],
+                datas[14],
+                datas[15],
+            });
+        }
+
+        public void read()
+        {
+            this.dataReceived(this, null);
+        }
+        public static string[] getPortNames()
+        {
+            return SerialPort.GetPortNames();
+        }
+    }
+}
---
+++ SmartParcelLockerController/fMain.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartParcelLockerController
+{
+    public partial class fMain : Form
+    {
+        Cu48b cu48b;
+        CheckBox[] _cbLockers;
+        CheckBox[] _cbSensors;
+
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void fMain_Load(object sender, EventArgs e)
+        {
+            _cbLockers = new CheckBox[48] {
+                cbLocker00, cbLocker01, cbLocker02, cbLocker03, cbLocker04, cbLocker05, cbLocker06, cbLocker07,
+                cbLocker08, cbLocker09, cbLocker10, cbLocker11, cbLocker12, cbLocker13, cbLocker14, cbLocker15,
+                cbLocker16, cbLocker17, cbLocker18, cbLocker19, cbLocker20, cbLocker21, cbLocker22, cbLocker23,
+                cbLocker24, cbLocker25, cbLocker26, cbLocker27, cbLocker28, cbLocker29, cbLocker30, cbLocker31,
+                cbLocker32, cbLocker33, cbLocker34, cbLocker35, cbLocker36, cbLocker37, cbLocker38, cbLocker39,
+                cbLocker40, cbLocker41, cbLocker42, cbLocker43, cbLocker44, cbLocker45, cbLocker46, cbLocker47
+            };
+
+            foreach (var checkBox in _cbLockers)
+            {
+                checkBox.Click += new EventHandler(_cbLockers_Clicked);
+                checkBox.CheckedChanged += new EventHandler(_cbLockers_CheckedChanged);
+            }
+
+            _cbSensors = new CheckBox[48]
+            {
+                cbSensor00, cbSensor01, cbSensor02, cbSensor03, cbSensor04, cbSensor05, cbSensor06, cbSensor07,
+                cbSensor08, cbSensor09, cbSensor10, cbSensor11, cbSensor12, cbSensor13, cbSensor14, cbSensor15,
+                cbSensor16, cbSensor17, cbSensor18, cbSensor19, cbSensor20, cbSensor21, cbSensor22, cbSensor23,
+                cbSensor24, cbSensor25, cbSensor26, cbSensor27, cbSensor28, cbSensor29, cbSensor30, cbSensor31,
+                cbSensor32, cbSensor33, cbSensor34, cbSensor35, cbSensor36, cbSensor37, cbSensor38, cbSensor39,
+                cbSensor40, cbSensor41, cbSensor42, cbSensor43, cbSensor44, cbSensor45, cbSensor46, cbSensor47,
+            };
+
+            uint[] baudRates = { 110, 150, 300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400, 460800, 921600 };
+            cbBaudRate.Items.Clear();
+            foreach (uint baudRate in baudRates)
+            {
+                cbBaudRate.Items.Add(baudRate.ToString());
+            }
+            if (cbBaudRate.Items.Count > 0) cbBaudRate.SelectedIndex = 7;
+
+            Form fMain = (Form)sender;
+            fMain.Text = Assembly.GetEntryAssembly().GetName().Name;
+        }
+
+        private void _cbLockers_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+
+        }
+
+        private void _cbLockers_Clicked(object sender, EventArgs e)
+        {
+            for (int index = 0; index < _cbLockers.Length; index++)
+            {
+                CheckBox cb = (CheckBox)sender;
+                if (cb.Name == _cbLockers[index].Name)
+                {
+                    try
+                    {
+                        if (cb.Checked) continue;
+                        cu48b.send(0, index, Cu48b.LOCKER_UNLOCK);
+                        String data = BitConverter.ToString(cu48b.dataOut);
+
+                        rtbPrint(data, Color.Black);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        rtbPrint(ex.Message, Color.Red);
+                    }
+
+                }
+            }
+        }
+
+        private void Cu48b_onDataReceived(object sender, EventArgs e)
+        {
+            Cu48b cU48B = (Cu48b)sender;
+            try
+            {
+                for (int index = 0; index < cU48B.lockers.Length; index++)
+                {
+                    _cbLockers[index].Invoke(new Action(() =>
+                    {
+                        _cbLockers[index].Checked = cU48B.lockers[index];
+                    }));
+                }
+                for (int index = 0; index < cU48B.sensors.Length; index++)
+                {
+                    _cbSensors[index].Invoke(new Action(() =>
+                    {
+                        _cbSensors[index].Checked = cU48B.sensors[index];
+                    }));
+                }
+
+            }
+            catch (Exception exception)
+            {
+                rtbOutput.Invoke(new Action(() =>
+                {
+                    rtbPrint(exception.Message, Color.Red);
+                }));
+            }
+            finally
+            {
+                String data = BitConverter.ToString(cU48B.dataIn);
+                rtbOutput.Invoke(new Action(() =>
+                {
+                    rtbPrint(data, Color.Green);
+                }));
+            }
+        }
+
+
+        private void bScanPortNames_Click(object sender, EventArgs e)
+        {
+            string[] portNames = Cu48b.getPortNames();
+            cbPortName.Items.Clear();
+            foreach (string portName in portNames)
+            {
+                cbPortName.Items.Add(portName);
+            }
+            if (cbPortName.Items.Count > 0) cbPortName.SelectedIndex = 0;
+        }
+
+
+        private void bGetStatus_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cu48b == null) return;
+                cu48b.send(0, 0, Cu48b.GET_STATUS);
+                cu48b.read();
+
+                String data = BitConverter.ToString(cu48b.dataOut);
+                rtbPrint(data, Color.Black);
+            }
+            catch (Exception ex)
+            {
+                rtbPrint(ex.Message, Color.Red);
+            }
+        }
+
+        void rtbPrint(String text, Color color)
+        {
+            rtbOutput.SelectionColor = color;
+            rtbOutput.SelectionFont = new Font("Courier", 11, FontStyle.Regular);
+            rtbOutput.AppendText(DateTime.Now.ToString("HH:mm:ss") + " | " + text + '\n');
+            rtbOutput.ScrollToCaret();
+        }
+
+        private void bUnlockAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cu48b == null) return;
+                cu48b.send(0, Cu48b.LOCKER_ALL, Cu48b.LOCKER_UNLOCK);
+
+                String data = BitConverter.ToString(cu48b.dataOut);
+                rtbPrint(data, Color.Black);
+            }
+            catch (Exception ex)
+            {
+                rtbPrint(ex.Message, Color.Red);
+            }
+        }
+
+
+        private void bClearOutput_Click(object sender, EventArgs e)
+        {
+            rtbOutput.Clear();
+        }
+
+        private void bConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cu48b == null && bConnect.Text.Equals("Connect"))
+                {
+                    cu48b = new Cu48b(cbPortName.Text, int.Parse(cbBaudRate.Text));
+                    cu48b.onDataReceived += Cu48b_onDataReceived;
+                    cu48b.open();
+                    bConnect.Text = "Disconnect";
+                    rtbPrint(String.Concat("Connected to ", cbPortName.Text), Color.Blue);
+                }
+                else if (cu48b != null && bConnect.Text.Equals("Disconnect"))
+                {
+                    cu48b.close();
+                    bConnect.Text = "Connect";
+                    rtbPrint(String.Concat("Disconnected to ", cbPortName.Text), Color.Blue);
+                }
+            }
+            catch (Exception ex)
+            {
+                rtbPrint(ex.Message, Color.Red);
+            }
+        }
+    }
+}
+
